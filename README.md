6bd802c902496e2fc57fa2753d9ac5a33bca1b53
superhan <chenchaohan@outlook.com>
2018-12-09 23:53:08 +0800

    build ntfs4mac

---
+++ README.md
@@ -0,0 +1,63 @@
+# NTFS X
+
+> 一个让你的 Mac 电脑能够读写 NTFS 的工具
+
+#### 软件界面
+
+![](/screen/mount_open.gif)
+
+![](/screen/language.gif)
+
+#### 资源地址
+
+DMG 下载地址：
+
+百度网盘： [https://pan.baidu.com/s/1skTMrU1](https://pan.baidu.com/s/1skTMrU1) 密码:dxmr
+
+备用地址：[https://pan.baidu.com/share/init?surl=skTMrU1](https://pan.baidu.com/share/init?surl=skTMrU1) 密码:dxmr
+
+#### Build Setup
+
+```bash
+# install dependencies
+yarn install
+
+# serve with hot reload at localhost:9080
+yarn run dev
+
+# build electron application for production
+yarn run build
+
+# run unit & end-to-end tests
+yarn test
+```
+
+---
+
+MIT License
+
+---
+
+## 安装 Electron
+
+```js
+cnpm install electron
+
+#查看 electron 版本
+electron -v
+```
+
+```js
+# 查看效果
+electron .
+```
+
+## 打包成软件包
+
+```js
+cnpm install electron-builder
+
+cnpm install electron-package
+```
+
+This project was generated with [electron-vue](https://github.com/SimulatedGREG/electron-vue)@[de85f81](https://github.com/SimulatedGREG/electron-vue/tree/de85f81890c01500113738bfe57bef136f9fbf52) using [vue-cli](https://github.com/vuejs/vue-cli). Documentation about the original structure can be found [here](https://simulatedgreg.gitbooks.io/electron-vue/content/index.html).
---
+++ src/renderer/components/About.vue
@@ -0,0 +1,123 @@
+<template>
+    <div>
+        <div v-if="$i18n.locale=='zh'">
+            <mu-row gutter>  
+                <mu-col class="header-title" width="70" tablet="70" desktop="70">
+                    <h3>让我们一起开发出更好用的软件！</h3>
+                </mu-col>
+            </mu-row>
+            <mu-row gutter > 
+                <div class="scan-icon">
+                        <mu-col width="35" tablet="35" desktop="35">
+                            <p>微信</p>   
+                            <img src="../assets/wx.png" width="150px" height="150px" alt="">
+                        </mu-col>
+                        <mu-col width="35" tablet="35" desktop="35">
+                            <p>支付宝</p>
+                            <img src="../assets/zfb.png" width="150px" height="150px" alt="">
+                        </mu-col>   
+                </div>
+            </mu-row>
+            <mu-row gutter>
+                <div class="tips">
+                    <ul>
+                        <li>该软件依赖Mac系统的diskutil、mount、umount、mount_ntfs、open等命令，
+        如果遇到问题，请先检查这些命令是否工作正常。</li>
+                        <li>该项目已经在github上开源，欢迎开发者加入，完善功能。
+        Github地址：<a href="javascript:void(0)" @click="open('https://github.com/Sailiy/ntfsx')">https://github.com/Sailiy/ntfsx</a>
+                        </li>
+                        <li>如果你想一起参与,请请发邮件到：<a href="mailto:chenchaohan@outlook.com">chenchaohan@outlook.com</a>
+                        </li>
+                        <li>感谢electron，vue，electron-vue，muse-ui，element-ui的开发者们。</li>
+                    </ul>
+                </div>  
+            </mu-row>
+        </div>
+
+<!-- 下面是英文版 -->
+
+        <div v-if="$i18n.locale=='en'">
+            <mu-row gutter>
+                <mu-col width="15" tablet="15" desktop="15"></mu-col>
+                <mu-col width="70" tablet="70" desktop="70">
+                    <h3>Please sponsor me so that I can develop better software</h3>
+                </mu-col>
+                <mu-col width="15" tablet="15" desktop="15"></mu-col>
+            </mu-row>
+            <mu-row gutter>
+                <mu-col width="15" tablet="15" desktop="15"></mu-col>
+                <mu-col width="35" tablet="35" desktop="35">
+                    &nbsp;&nbsp;WeChat<br>
+                    <img src="../assets/wx.png" width="150px" height="150px" alt="">
+                </mu-col>
+                <mu-col width="35" tablet="35" desktop="35">
+                    &nbsp;&nbsp;AliPay<br>
+                    <img src="../assets/zfb.png" width="150px" height="150px" alt="">
+
+                </mu-col>
+                <mu-col width="15" tablet="15" desktop="15"></mu-col>
+            </mu-row>
+            <mu-row gutter>
+                <mu-col width="15" tablet="15" desktop="15"></mu-col>
+                <mu-col width="70" tablet="70" desktop="70">
+               <pre>
+The project relies on the Mac system diskutil, mount, umount, mount_ntfs, open, command line tool,If you have problems, check to see if these tools are working properly.
+                   <br>
+The project has been open source on github and welcomes developers to join and improve functionality.
+Making address: <a href="javascript:void(0)" @click="open('https://github.com/Sailiy/ntfsx')">https://github.com/Sailiy/ntfsx</a>
+                   <br>
+My ability is not only that, if you can give me good job opportunities.
+Please email to: <a href="mailto:sailiy@126.com">sailiy@126.com</a>
+                   <br>
+Thanks to the developers of electron, vue, electronic-vue, Muse-UI, element-UI.
+               </pre>
+                </mu-col>
+                <mu-col width="15" tablet="15" desktop="15"></mu-col>
+            </mu-row>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  methods: {
+    open(link) {
+      this.$electron.shell.openExternal(link);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.header-title {
+  flex: 1;
+}
+.header-title h3 {
+  padding-left: 4em;
+}
+.scan-icon {
+  display: flex;
+  flex: 1;
+}
+.scan-icon div {
+  flex: 1;
+  text-align: center;
+}
+.scan-icon div > p {
+  text-align: center;
+}
+
+.tips {
+  display: flex;
+}
+
+.tips li {
+  line-height: 2em;
+}
+</style>
